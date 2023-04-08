4ce1d2b6ffcfd44db3235a4c1074d2f7e4e056f2
alzamazfar <130217550+alzamazfar@users.noreply.github.com>
2023-04-08 17:37:06 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,227 @@
+basic.forever(function () {
+    for (let index = 0; index < 2; index++) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . . #
+                . . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . . #
+                . . . . #
+                . . . . #
+                . . . . .
+                . . . . .
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . . #
+                . . . . #
+                . . . . #
+                . . . . #
+                . . . . .
+                `)
+        }
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . . . #
+                . . . . #
+                . . . . #
+                . . . . #
+                . . . . #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . # . #
+                . . . . #
+                . . . . #
+                . . . . #
+                . . . . #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . # . #
+                . # # # #
+                . . . . #
+                . . . . #
+                . . . . #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . . #
+                . . # . #
+                . # # # #
+                . . . . #
+                . . . . #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . . #
+                . . . . #
+                . . # . #
+                . # # # #
+                . . . . #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . . #
+                . . . . #
+                . . . . #
+                . . # . #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . # #
+                . . . . #
+                . . . . #
+                . . # . #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . # # #
+                . . . # #
+                . . . . #
+                . . # . #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . . #
+                . . # # #
+                . . . # #
+                . . # . #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . . #
+                . . . . #
+                . . # # #
+                . . # # #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . # . . #
+                . . . . #
+                . . # # #
+                . . # # #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 0; index++) {
+            basic.showLeds(`
+                . # . . #
+                . # . . #
+                . . # # #
+                . . # # #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . . #
+                . # . . #
+                . # # # #
+                . . # # #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . . #
+                . . . . #
+                . # # # #
+                . # # # #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                # . . . #
+                . . . . #
+                . # # # #
+                . # # # #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                # . . . #
+                # . . . #
+                . # # # #
+                . # # # #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                # . . . #
+                # . . . #
+                # # # # #
+                . # # # #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                # . . . #
+                # . . . #
+                # # # # #
+                # # # # #
+                . # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                # . . . #
+                # . . . #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+        }
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . #
+                # . . . #
+                `)
+        }
+        for (let index = 0; index < 5; index++) {
+            basic.showLeds(`
+                . . . . .
+                . . . . #
+                . . . # .
+                # . # . .
+                . # . . .
+                `)
+        }
+    }
+})
